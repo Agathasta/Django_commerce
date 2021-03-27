336ce3107dd4773a827f95739f2ad6104463f93c
agathasta <kalidonia@hotmail.com>
2021-03-27 18:28:46 +0100

    Add basic models (Listing, Bid, Comment)

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,20 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='listings')
+    title = models.CharField(max_length=32)
+    description = models.CharField(max_length=640)
+    start_bid = models.DecimalField(max_digits=7, decimal_places=2)
+    image_url = models.URLField(max_length=200, blank=True)
+
+class Bid(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='bids')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bids')
+    bid = models.DecimalField(max_digits=7, decimal_places=2)
+
+class Comment(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    comment = models.TextField()
